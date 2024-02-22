672516ec056121c064b921558175980b451ed740
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-22 11:54:44 -0300

    Segunda aula de REACT

---
+++ 22.02.24/aula02/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Comp from './Comp'
+
+export default function App(){
+  return (
+    <>
+      <h1 style={{fontSize: "2cm", color:"green"}}>Hello World!!!</h1>
+      <Comp/ >
+    </>
+  )
+}
---
+++ 22.02.24/aula02/src/Comp.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function Comp(){
+    let parag = {
+        color: "blue",
+        fontSize: "3cm"
+    };
+  return (
+    <>
+      <h1 style={ parag }>Novo Componente</h1>
+    </>
+  )
+}
