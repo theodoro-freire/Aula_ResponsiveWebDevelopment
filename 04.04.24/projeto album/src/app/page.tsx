c5127a282349b2376e186122c759cbcdf5623ab2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-04 11:57:57 -0300

    Projeto Album em Next/REACT

---
+++ 04.04.24/projeto album/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Card from "./card/card"
+import Carrossel from "./carrossel/carrossel"
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Carrossel />
+      <Card /> 
+    </>
+  );
+}
